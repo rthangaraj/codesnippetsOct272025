3d5c70ac019f4c51b42e1bb578c5179f6bbcfa73
Ramesh Thangaraj <ramesh.thangaraj@gmail.com>
2025-10-27 23:30:20 +0000

    Update main.tf

--- terraform/networking/main.tf
+++ terraform/networking/main.tf
@@ -5,7 +5,6 @@ provider "azurerm" {
 module "vnet" {
   source = "./modules/vnet"
   vnets  = var.vnets
-  ddos_protection_plan_name = var.ddos_protection_plan_name
-  ddos_protection_plan_enabled = var.ddos_protection_plan_enabled
+
 
 }
